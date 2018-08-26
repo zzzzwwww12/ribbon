65b345751e8d728050336fb07ceec055648ac670
zzzzwwww12 <zhouruizhe2016@126.com>
2018-08-26 22:40:35 +0800

    ribbon + restTemplate init version

---
+++ src/main/java/com/frank/ribbon/server/RibbonConfiguration.java
@@ -0,0 +1,15 @@
+package com.frank.ribbon.server;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RibbonConfiguration {
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
